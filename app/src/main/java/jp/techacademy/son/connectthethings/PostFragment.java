0d969f05c81a19f453fd9f9ae2b4d4b22b609e05
sonyanai <taison0930@outlook.jp>
2018-04-03 18:31:36 +0900

    postButton login連携 ログインした時にuserRefに登録 searchButton追加 postfragmentのレイアウト
    closeButton背景無色にする

--- app/src/main/java/jp/techacademy/son/connectthethings/PostFragment.java
+++ app/src/main/java/jp/techacademy/son/connectthethings/PostFragment.java
@@ -1,17 +1,13 @@
 package jp.techacademy.son.connectthethings;
 
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
-
-import com.google.firebase.auth.FirebaseAuth;
+import android.widget.ImageView;
 
 /**
  * Created by taiso on 2018/04/03.
@@ -20,12 +16,19 @@ import com.google.firebase.auth.FirebaseAuth;
 public class PostFragment extends Fragment {
     public static final String TAG = "PostFragment";
 
+    ImageView selectedImageView;
+    EditText contentsEditText;
+    Button sendButton;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View v = inflater.inflate(R.layout.fragment_post,container,false);
 
+        selectedImageView = (ImageView)v.findViewById(R.id.selectedImage);
+        contentsEditText = (EditText)v.findViewById(R.id.contentsEditText);
+        sendButton = (Button)v.findViewById(R.id.sendButton);
 
         return v;
     }
